3c90a34ca93bf8893e6a56b6c4ba89124eb74491
Maxim grishkov <grishkov.mn@gmail.com>
2014-03-05 00:38:38 +0400

    добавил шаблон диалогового окна редактирования

---
+++ Application/BeautySmileCRM/Interfaces/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySmileCRM.Interfaces
+{
+    public interface IDialogUserControl
+    {
+        event EventHandler AllowSaveChanged;
+
+        bool AllowSave { get; set; }
+        bool Bind<T>(T ID);
+        bool Save();
+    }
+}
