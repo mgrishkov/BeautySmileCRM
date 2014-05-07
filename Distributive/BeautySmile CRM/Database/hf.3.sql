1332af9a76b1162c2e38bc5ecd6194264e1c40b0
Maxim grishkov <grishkov.mn@gmail.com>
2014-05-08 00:27:29 +0400

    * <-

---
+++ Distributive/BeautySmile CRM/Database/hf.3.sql
@@ -0,0 +1,16 @@
+﻿insert into ADM.Privilege (ID, Name, Description, GroupID)
+values (34, 'CreateService', 'Создание услуги', 2),
+       (35, 'ModifySerice', 'Изменение услуги', 2),
+       (36, 'DeleteService', 'Удаление услуги', 2),
+       (37, 'ViewService', 'Просмотр правочника услуги', 2);
+go
+insert into ADM.UserPrivilege (UserID, PrivilegeID)
+values (5, 34),
+       (5, 35),
+       (5, 36),
+       (5, 37),
+       (8, 34),
+       (8, 35),
+       (8, 36),
+       (8, 37);
+go
